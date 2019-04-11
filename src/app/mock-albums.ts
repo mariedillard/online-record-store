9107aa1e8d75131c5e65912805c5f767145d8fa2
Student <example@example.com>
2019-04-11 16:38:17 -0700

    completes up to firebase: updating entries

---
+++ src/app/mock-albums.ts
@@ -0,0 +1,14 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] = [
+    new Album("Disco Volante (1995)", "Mr.Bungle", "The second studio album by American experimental rock band Mr.Bungle."),
+    new Album("Bellybutton (1990)", "Jellyfish", "The debut album by American rock band Jellyfish."),
+    new Album("Congratulations (2010)", "MGMT", "The second studio album by American rock band MGMT."),
+    new Album("Hissing Fauna, Are You The Destroyer? (2007)", "of Montreal", "The eighth album by American indie pop band of Montreal."),
+    new Album("Transgender Dysphoria Blues (2014)", "Against Me!", "The sixth studio album by punk band Against Me!"),
+    new Album("Now You Are One Of Us (2006)", "the pAper chAse", "The fourth album by alternative rock band the pAper chAse."),
+    new Album("Pop Food (2017)", "Jack Stauber", "The third album by American indie pop musician Jack Stauber."),
+    new Album("It's A Wonderful Life (2001)", "Sparklehorse", "The third studio album by American musical act Sparklehorse."),
+    new Album("Angel Dust (1992)", "Faith No More", "The fourth studio album by American rock band Faith no More."),
+    new Album("I Brought You My Bullets, You Brought Me Your Love (2002)", "My Chemical Romance", "The debut studio album by American rock band My Chemical Romance"),
+  ];
